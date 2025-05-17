a6c0ba245e6bd05ddc82c955106efa8fa3db2b67
Cristhian036 <koenigseggagera036@gmail.com>
2025-05-17 10:00:57 -0500

    feat: Entidad Usuario y Rol

---
+++ sistema_municipal_backend/src/main/java/com/utp/sistema_municipal_backend/repositorios/RolRepository.java
@@ -0,0 +1,7 @@
+package com.utp.sistema_municipal_backend.repositorios;
+
+import com.utp.sistema_municipal_backend.modelo.Rol;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RolRepository extends JpaRepository<Rol,Long> {
+}
