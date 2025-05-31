3df2305540ff8b50cfb56605048c2a428e7016d4
YinyerAle05 <U22218751@utp.edu.pe>
2025-05-31 14:01:39 -0500

    Página para gestión principal como agregar y editar las multas

---
+++ sistema_municipal_frontend/src/app/crud-multa/crud-multa.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-crud-multa',
+  templateUrl: './crud-multa.component.html',
+  styleUrls: ['./crud-multa.component.css']
+})
+export class CrudMultaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
